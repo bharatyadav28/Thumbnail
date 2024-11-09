1f5576cd853f4c242efbf3279df1287aa6a97c27
Bharat Yadav <yadavbharat386@gmail.com>
2024-11-09 11:21:54 +0530

    Url updated

--- src/app/webp/page.js
+++ src/app/webp/page.js
@@ -4,7 +4,7 @@ export async function generateMetadata({ params }) {
   // Fetch the video data
 
   // Thumbnail URL Generated
-  const thumbnailUrl = `https://dewv7gdonips4.cloudfront.net/uploads/user-668e5194d73df6053315bb41/profile/1726149063451-16.RepublicMovieDirectorDEVAKATTA[Talk]withStringVinodh_Promo-(1080p).web p.webp`;
+  const thumbnailUrl = `https://dewv7gdonips4.cloudfront.net/uploads/user-668e5194d73df6053315bb41/profile/1726149063451-16.RepublicMovieDirectorDEVAKATTA[Talk]withStringVinodh_Promo-(1080p).jpg.webp`;
 
   console.log("thumbnailUrl", thumbnailUrl);
 
