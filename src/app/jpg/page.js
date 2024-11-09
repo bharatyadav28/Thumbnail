ed80dec028107144b0ee601fdcc4d6695be203c4
Bharat Yadav <yadavbharat386@gmail.com>
2024-11-09 11:07:37 +0530

    initial commit

---
+++ src/app/jpg/page.js
@@ -0,0 +1,55 @@
+import React from "react";
+
+export async function generateMetadata({ params }) {
+  // Fetch the video data
+
+  // Thumbnail URL Generated
+  const thumbnailUrl = `https://dewv7gdonips4.cloudfront.net/uploads/user-668e5194d73df6053315bb41/profile/1726149063451-16.RepublicMovieDirectorDEVAKATTA[Talk]withStringVinodh_Promo-(1080p).jpg.webp`;
+
+  console.log("thumbnailUrl", thumbnailUrl);
+
+  return {
+    title: "Video -webp ",
+    description: "Web p image",
+    openGraph: {
+      type: "video.other",
+      title: "Web p image",
+      description: "this is a webp image",
+      images: [
+        {
+          url: thumbnailUrl,
+          width: 1200, // Set standard width
+          height: 630, // Set standard height
+        },
+      ],
+      // url: `https://your-domain.com/video/${video.id}`,
+      // videos: [
+      //   {
+      //     url: video.video_url,
+      //     width: 1280,
+      //     height: 720,
+      //     type: "video/mp4",
+      //   },
+      // ],
+    },
+    twitter: {
+      card: "player",
+      title: "Web p image",
+      description: "this is a webp image",
+      images: [thumbnailUrl],
+      //   player: video.video_url,
+      playerWidth: 1280,
+      playerHeight: 720,
+    },
+  };
+}
+
+function WebpImagePage() {
+  return (
+    <div>
+      <p>This is a webp image page</p>
+    </div>
+  );
+}
+
+export default WebpImagePage;
---
+++ src/app/webp/page.js
@@ -0,0 +1,55 @@
+import React from "react";
+
+export async function generateMetadata({ params }) {
+  // Fetch the video data
+
+  // Thumbnail URL Generated
+  const thumbnailUrl = `https://res.cloudinary.com/djntj8ck7/image/upload/v1691730500/Intensify/Courses/tmp-2-1691730492228_nyvzem.jpg`;
+
+  console.log("thumbnailUrl", thumbnailUrl);
+
+  return {
+    title: "Video -jpg ",
+    description: "Web p image",
+    openGraph: {
+      type: "video.other",
+      title: "Web p image",
+      description: "this is a jpg image",
+      images: [
+        {
+          url: thumbnailUrl,
+          width: 1200, // Set standard width
+          height: 630, // Set standard height
+        },
+      ],
+      //   url: `https://your-domain.com/video/${video.id}`,
+      //   videos: [
+      //     {
+      //       url: video.video_url,
+      //       width: 1280,
+      //       height: 720,
+      //       type: "video/mp4",
+      //     },
+      //   ],
+    },
+    twitter: {
+      card: "player",
+      title: "Web p image",
+      description: "this is a jpg image",
+      images: [thumbnailUrl],
+      //   player: video.video_url,
+      playerWidth: 1280,
+      playerHeight: 720,
+    },
+  };
+}
+
+function jpgImagePage() {
+  return (
+    <div>
+      <p>This is a webp image page</p>
+    </div>
+  );
+}
+
+export default jpgImagePage;
